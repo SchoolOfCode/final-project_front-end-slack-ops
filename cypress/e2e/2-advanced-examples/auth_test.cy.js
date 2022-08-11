84d45d3547cb922c116372ac3071dd41b4e1ba89
sibowen535 <bowen.simon@gmail.com>
2022-08-11 14:06:18 +0100

    cypress e2e basic files

---
+++ cypress/e2e/2-advanced-examples/auth_test.cy.js
@@ -0,0 +1,13 @@
+it("throws a cross origin error", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("#qsLoginBtn").click();
+    
+    cy.origin("https://myusername.auth0.com/", () => {
+    cy.get("#1-email").type(Cypress.env("EMAIL"));
+    cy.get("input[type='password']")
+      .type(Cypress.env("PASSWORD"));
+    cy.get("button[type='submit']").click()
+  },
+  
+  cy.get("h1").should("contain", "React.js Sample Project"))
+  });
