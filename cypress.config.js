6438d9438005e4786f9f80c838621b1b7666815d
sibowen535 <bowen.simon@gmail.com>
2022-08-11 11:11:02 +0100

    amending the cypress json

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,10 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    {
+        "experimentalSessionAndOrigin"; true
+    }
     },
   },
 });
+
